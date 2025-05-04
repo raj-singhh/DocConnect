c5dd6ce1a33f3f3449faefd8ff30d9526b3a0874
Raj Singh <anjaypaliwal26@gmail.com>
2025-05-05 00:46:41 +0530

    some changes in page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -235,7 +235,7 @@ export default function DestinationPage() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
                       <h3 className="mt-2 text-lg font-medium text-gray-900">No doctors found</h3>
-                      <p className="mt-1 text-gray-500">We couldn't find any doctors matching your criteria</p>
+                      <p className="mt-1 text-gray-500">We couldn&apos;t find any doctors matching your criteria</p>
                       <div className="mt-6">
                         <button
                           onClick={() => {
