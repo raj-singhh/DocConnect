3ea9fd24628fda35c97d6d2cebd2461016cce4bc
Raj Singh <anjaypaliwal26@gmail.com>
2025-05-05 19:49:14 +0530

    fix bug

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:  'https://docconnect-five.vercel.app/',
+  origin:  'https://docconnect-five.vercel.app',
   methods: ["GET", "POST", "PUT", "DELETE"],
  
 })); 
